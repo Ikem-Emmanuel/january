14053d0cebeabc3b62a6c429effcf727d7b982d7
emmanuel-ikem <ikem43@gmail.com>
2024-01-02 17:10:49 +0100

    ci: project folder setup and fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,9 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
-}
+	title: "DevKodes",
+	description: "Next.js starter app",
+};
 
 export default function RootLayout({ children }) {
   return (
